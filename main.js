fd9cc8f4326d1e250a785935a69aa0a8e2ec5c89
Michał Kawczak <mkawczak00@gmail.com>
2023-07-11 01:42:26 +0200

    Add mobileNav in header

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-$(document).ready(function() {
-    var endDiv = $('#end');
-  
-    function isAtBottom() {
-      var scrollTop = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      var documentHeight = $(document).height();
-      var scrollBottom = scrollTop + windowHeight;
-      var endDivBottom = endDiv.offset().top + endDiv.outerHeight();
-  
-      return scrollBottom >= endDivBottom;
-    }
-  
-    function animateOnScroll() {
-      if (isAtBottom()) {
-        endDiv.addClass('animate');
-      } else {
-        endDiv.removeClass('animate');
-      }
-    }
-  
-    $(window).scroll(animateOnScroll);
-  });
+const nav = document.querySelector('#navMenu');
+const burger = document.querySelector('#switch');
+
+burger.addEventListener('click', (e) => 
+{
+  nav.classList.toggle('activeMenu');
+  burger.classList.toggle('activeBurger');
+});
