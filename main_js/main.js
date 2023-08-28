31ab7caa1ebbab2c5e4b4a774714dde3cbfa4f4f
Michał Kawczak <mkawczak00@gmail.com>
2023-08-29 00:20:16 +0200

    Repair last update, do download/print, animiation loaded, easter eage, root, and form

--- main_js/main.js
+++ main_js/main.js
@@ -7,34 +7,49 @@ const start_JS = new Date('2023-08-20');
 const coddingHTML = document.getElementById('HTML-CSS');
 const coddingJS = document.getElementById('JS');
 
-function updateTimer(counter, startTime) {
+let lastUpdate = Date.now();
+
+function updateTimer(counter, startTime, lastUpdate) {
     const today = new Date();
     const timeDifference = today.getTime() - startTime.getTime();
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     counter.innerHTML = days + ` days`;
+
+    const currentTime = Date.now();
+    const timeLastUpdate = currentTime - lastUpdate;
+    lastUpdate = currentTime;
+
+    return timeLastUpdate / 1000; // Zwracamy czas w sekundach
 }
 
-const updateHTMLTimer = () => updateTimer(coddingHTML, start_HTML);
-const updateJSTimer = () => updateTimer(coddingJS, start_JS);
+const updateHTMLTimer = () => {
+    const secondsLastUpdate = updateTimer(coddingHTML, start_HTML, lastUpdate);
+    lastUpdate = Date.now();
+    return secondsLastUpdate;
+};
+
+const updateJSTimer = () => {
+    const secondsLastUpdate = updateTimer(coddingJS, start_JS, lastUpdate);
+    lastUpdate = Date.now();
+    return secondsLastUpdate;
+};
 
 updateHTMLTimer();
 updateJSTimer();
 
-setInterval(updateHTMLTimer, 500);
-setInterval(updateJSTimer, 500);
+setInterval(updateHTMLTimer, 86400000);
+setInterval(updateJSTimer, 86400000);
 
 ////////////////////////////////////////////////////////////////////////////// 
-//Zmiana zawartosci diva liczników na informacje o ostateniej aktualizacji 
+//Zmiana zawartosci diva liczników na informacje o ostatniej aktualizacji 
 
 const divFrame = document.getElementById('divFrame');
 const original_frame = divFrame.innerHTML;
 
 divFrame.addEventListener('mouseover', () => {
-    divFrame.innerHTML = '<div style="display: flex; align-items: center;"> Hi </div>';
+    const lastIntervalCallInfoHTML = updateHTMLTimer();
+    divFrame.innerHTML = '<div class="last_update">Last update: ' + lastIntervalCallInfoHTML + ' seconds ago</div>';
 });
 divFrame.addEventListener('mouseout', () => {
     divFrame.innerHTML = original_frame;
 });
-
-
-
